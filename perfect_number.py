1fdf8f91866aa60b590beba16dd9b3a7af52d93a
Madhuri <60868865+learn-programs@users.noreply.github.com>
2020-08-20 18:39:20 +0530

    Update perfect_number.py

--- perfect_number.py
+++ perfect_number.py
@@ -1,10 +1,17 @@
 # to find a number is perfect number or not.
-# sum of factors of a number is equal to number itself,
-# that number is called the perfect number.
+#
 
 
 def is_perfect(num):
-
+	"""
+	is_perfect(number)
+	
+	returns a number is Perfect number or Not.
+	
+	sum of factors of a number is equal to number itself,
+	that number is called the perfect number.
+	"""
+	
 	factors = []
 	for i in range(1, num):
 		if num % i == 0:
@@ -14,7 +21,6 @@ def is_perfect(num):
 
 	if num == sum_:
 		print("The {} is perfect number.".format(num))
-
 	else:
 		print("The {} is not perfect number.".format(num))
 
