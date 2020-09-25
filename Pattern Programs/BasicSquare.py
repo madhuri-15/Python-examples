bde1df26b4dc295b309202676bbdae0af8514dbc
Madhuri <60868865+learn-programs@users.noreply.github.com>
2020-09-25 16:53:30 +0530

    Create BasicSquare.py

---
+++ Pattern Programs/BasicSquare.py
@@ -0,0 +1,14 @@
+# Python program to print square pattern.
+
+n = 5 # number of rows
+for i in range(n):
+	for j in range(n):
+		print("*", end=" ")
+	print(" ")
+
+
+# Ouput:
+* * * * *  
+* * * * *  
+* * * * *  
+* * * * *  
