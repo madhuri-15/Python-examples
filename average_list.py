abe38aac9eccccce86051ff7ff9a092f6377ddb7
Madhuri Patil <60868865+MadhurixPatil@users.noreply.github.com>
2021-05-09 15:21:37 +0530

    Update average_list.py

--- average_list.py
+++ average_list.py
@@ -1,17 +1,22 @@
 # Python program to calculate average of numbers in a given list.
 
-def avg_list(list_):
-    
-    return sum(list_)/len(list_)
+
+def average_of_list(list_):
+	avg = sum(list_)/len(list_)
+	return avg
 
 list_ = [1, 2, 4, 5]
 
 #to get list from user.
 input_list = []
-n = 10 #int(input('Total number of elements in a list: '))
-for i in range(10):
+
+n = int(input('Total number of elements in a list: '))
+
+for i in range(n):
 	elem = int(input('enter a number: '))
 	input_list.append(elem)
 
+average = average_of_list(input_list))
+print(average)
 
-print(avg_list(input_list))
+
